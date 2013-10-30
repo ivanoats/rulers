d17efd26bad7d2e473667ee82902e0f8527fd10a
Ivan Storck <ivanoats@gmail.com>
2013-10-29 22:02:11 -0700

    exception handling

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -10,7 +10,17 @@ module Rulers
       end
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      text = controller.send(act)
+      begin
+        text = controller.send(act)
+      rescue Exception => e
+        text = "<!doctype html><html><head></head><body>"
+        text = "Oops! A #{e.class}:#{e.message} exception happened! <br>\n"
+        text += "<ul>"
+        e.backtrace.each do |line|
+          text += "<li>#{line}</li>"
+        end
+        text += "</ul></body></html>"
+      end
       [200, {'Content-Type' => 'text/html'},
         [text]]
     end
