c37e78988f13cdb0c45f374d4004385ecdfbdf13
Ivan Storck <ivanoats@gmail.com>
2013-11-12 10:28:42 -0800

    use file separator instead of slash

--- lib/rulers/util.rb
+++ lib/rulers/util.rb
@@ -1,9 +1,9 @@
 module Rulers
   def self.to_underscore(string)
-    my_string = string.gsub(/::/,'/').
-      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-      gsub(/([a-z\d])([A-Z])/,'\1_\2').
-      tr("-","_").downcase
+    my_string = string.gsub(/::/, File::SEPARATOR).
+      gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
+      gsub(/([a-z\d])([A-Z])/, '\1_\2').
+      tr("-", "_").downcase
     puts my_string
     my_string
   end
