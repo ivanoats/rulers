d08cd66e2cdbd435198497491ca6762a69fa7e62
Ivan Storck <ivanoats@gmail.com>
2013-11-12 10:33:37 -0800

    sum verbose method

--- lib/rulers/array.rb
+++ lib/rulers/array.rb
@@ -2,4 +2,11 @@ class Array
   def sum(start = 0)
     inject(start, &:+)
   end
+
+  def sum_verbose(sum = 0)
+    reduce do |sum, n|
+      puts "I'm reducing  #{sum} with #{n} !!"
+      sum + n
+    end
+  end
 end
